5d6117896d7ef0de1791e9d8f16531f98486e892
unknown <anlcnert@gmail.com>
2022-09-13 21:10:38 +0300

    second

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var _ = require("underscore");
-var path = require("path");
+import express from "express";
 var app = express();
 var port = 9999;
+app.use(express.json());
 
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-
-
-app.get("/",function(req,res){
+app.get("/",() => {
     res.sendFile(path.join(__dirname, 'home.html'))
 })
 
-app.post("/giris",function(req,res){
-    let body = _.pick(req.body, "username","pw");
-    if(body.username == "anilcan"){
-         if(body.pw == "anilcan")
-         {
-            res.send("Merhaba "+body.username);
-         }
-         else{
-            res.send("Hatalı parola");
-         }
-    }
-    else{
-        res.send("Kullanıcı adı hatalı");
-    }
-   
-})
 
-app.post("/kaydol",function(req,res){
-    let body = _.pick(req.body, "username","pw","ad","soyad");
-    res.send(body.username);
-})
-
-app.listen(port,function(req,res){
-
+app.listen(port, () => {
     console.log('Servis aktif: http://localhost:%s', port);
 });
