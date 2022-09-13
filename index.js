6dc9d33e840f92f77eb2eb9121787f82ff68d0bb
unknown <anlcnert@gmail.com>
2022-09-13 21:55:24 +0300

    130922-21:55

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
+import _ from "underscore";
+import path from "path";
+
+
 var app = express();
 var port = 9999;
-app.use(express.json());
 
+app.use(express.json());
+app.use('/public', express.static(path.join('.', 'public')));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    //res.send("Hello world");
+    res.sendFile('/home.html', { root: '.' })
 })
 
 
